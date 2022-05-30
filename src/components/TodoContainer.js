42845ef6e1253987671a2dac214a0f9d58c82905
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-05-30 15:33:32 +0100

    Modified the js files to implement the delete function

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,5 +1,6 @@
 import React from "react"
 import TodoList from "./TodoList";
+import Header from "./Header";
 
 class TodoContainer extends React.Component {
     state = {
@@ -20,12 +21,40 @@ class TodoContainer extends React.Component {
                 completed: false,
             }
         ],
-    };    
+  };    
+
+  handleChange = (id) => {
+    this.setState(prevState => ({
+      todos: prevState.todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo, completed: !todo.completed,
+          }
+        }
+        return todo;
+      })
+    }));
+  };
+
+  delTodo = id => {
+    this.setState({
+      todos: [
+        ...this.state.todos.filter(todo => {
+          return todo.id !== id;
+        })
+      ]
+    });
+  };
 
   render() {
     return (
       <div>
-        <TodoList todos = {this.state.todos} />
+        <Header />
+        <TodoList 
+        todos = {this.state.todos} 
+        handleChangeProps={this.handleChange} 
+        delTodoProps={this.delTodo}
+        />
       </div>
     )
   }
