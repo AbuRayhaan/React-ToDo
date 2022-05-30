a37cb95bb763e0db9144e4b8bf63e3bf9f43bcab
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-05-30 14:25:14 +0100

    Modified the TodoContainer.js file

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,33 @@
+import React from "react"
+import TodoList from "./TodoList";
+
+class TodoContainer extends React.Component {
+    state = {
+        todos: [
+            {
+                id: 1,
+                title: "Setup Development environment",
+                completed: true,
+            },
+            {
+                id: 2,
+                title: "Develop website and add content",
+                completed: false,
+            },
+            {
+                id: 3,
+                title: "Deploy the live server",
+                completed: false,
+            }
+        ],
+    };    
+
+  render() {
+    return (
+      <div>
+        <TodoList todos = {this.state.todos} />
+      </div>
+    )
+  }
+}
+export default TodoContainer
