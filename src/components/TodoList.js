3ce171808d58660b04fe26dead9388e1a3d626ac
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-05-30 19:55:46 +0100

    Implemented the Edit feature

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -11,6 +11,7 @@ class TodoList extends React.Component {
                 todo={todo} 
                 handleChangeProps={this.props.handleChangeProps}
                 delTodoProps={this.props.delTodoProps}
+                setUpdate={this.props.setUpdate}
                 />
                 ))}
             </ul>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom"
 //component file
 import TodoContainer from "./components/TodoContainer"
 //stylesheet
-import './index.css'
+import './index.scss'
 
 ReactDOM.render(
   <React.StrictMode>
